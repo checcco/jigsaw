4b721ff0b3d8c4a1b0bd94b8cb6f562417c44802
Francesco Pinto <checcco@MacBook-Pro-di-Francesco.local>
2012-11-16 16:13:53 +0100

    changed greeting

--- info.php
+++ info.php
@@ -1,1 +1,19 @@
-<?php echo phpinfo(); ?>
+<?php 
+
+$host = "ec2-54-243-93-37.compute-1.amazonaws.com:5432"; 
+$user = "nfannucmgavuaw"; 
+$pass = "dtTXZ58LPKTVRupz6rvOu4ZdiQ"; 
+$db = "d61mf9vhkn48ck"; 
+
+$con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+    or die ("Could not connect to server\n"); 
+
+$query = "SELECT VERSION()"; 
+$rs = pg_query($con, $query) or die("Cannot execute query: $query\n"); 
+$row = pg_fetch_row($rs);
+
+echo $row[0] . "\n";
+
+pg_close($con); 
+
+?>
